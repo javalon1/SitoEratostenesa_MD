740ddb41b8adb318cfdc2af6dd4d485f63453ddd
Maciej Drobot <maciejdrobot@gmail.com>
2019-05-13 22:22:14 +0100

    Done

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,42 +1,41 @@
 package com.sda.algorytmy;
 
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj dlugosc zbioru (wieksza niz 2): ");
         int dlugoscZbioru = scanner.nextInt();
-        while (dlugoscZbioru < 2) {
+
+        //Gdyby ktos podal mniejsza niz 2
+        while (dlugoscZbioru <= 2) {
             System.out.println("Podales za mala liczbe, sproboj ponownie: ");
             dlugoscZbioru = scanner.nextInt();
         }
+
         boolean[] tablica = new boolean[dlugoscZbioru + 1];
         for (int i = 2; i < dlugoscZbioru + 1; i++) {
             tablica[i] = true;
         }
 
-
-        for(int i = 2; i < dlugoscZbioru + 1; i++) {
+        for (int i = 2; i < dlugoscZbioru + 1; i++) {
             int wielokrotnosc = i + i;
-            while (wielokrotnosc < dlugoscZbioru + 1){
+            while (wielokrotnosc < dlugoscZbioru + 1) {
                 tablica[wielokrotnosc] = false;
                 wielokrotnosc += i;
             }
         }
 
-        System.out.println(Arrays.toString(tablica));
+        System.out.println("W podanym zbiorze liczby pierwsze (uzyskane metoda Erastotenesa) to: ");
 
-
-
-
-
-
-
-
-
-
+        for (int i = 2; i < dlugoscZbioru + 1; i++) {
+            if (tablica[i] == true) {
+                System.out.print(i);
+                System.out.print("  ");
+            }
+        }
     }
 }
